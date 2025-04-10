7b3f17ce586f03091af61d5c21579a75885e7cbe
Carlos Rodrigo <rodrigo.darkcode@gmail.com>
2025-04-10 14:04:19 -0400

    Subir estructura de proyecto React y componente VerificarCertificado

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import VerificarCertificado from './components/VerificarCertificado';
+
+function App() {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <VerificarCertificado codigo="demo123" />
+    </div>
+  );
+}
+
+export default App;
